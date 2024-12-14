391d3788b4f4c3c06870c9f09146e8b147615780
Halim <UG_31159635@ics.tanta.edu.eg>
2024-12-14 21:10:16 +0200

    Make startUP page  Beafore Login page .

--- testt/StartUp.cs
+++ testt/StartUp.cs
@@ -1,13 +1,39 @@
+using System;
+using System.Windows.Forms;
+using System.Windows.Forms;
+
 namespace testt
 {
     public partial class StartUp : Form
     {
+        private System.Windows.Forms.Timer timer;
+
         public StartUp()
         {
             InitializeComponent();
+
+            timer = new System.Windows.Forms.Timer();
+            timer.Interval = 2000;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+
+        }
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            timer!.Stop();// End Timer After Complete The Time
+
+            Login log = new Login();
+            log.Show();
+            this.Hide();
         }
 
+
         private void StartUp_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
         {
 
         }
